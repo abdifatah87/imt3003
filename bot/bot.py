a89d1697fbc14ae5fd1950f7789c814f8ce68aa2
jimbob <abdifatah87@live.no>
2020-01-18 17:05:54 +0100

    kan sende info til forskjellige kanaler

--- bot/bot.py
+++ bot/bot.py
@@ -1,14 +1,24 @@
 import discord, os
 from discord.ext import commands
+import aiohttp
+import asyncio
+import json
 
+client1 = discord.Client()
 client = commands.Bot(command_prefix = '.')
 
 @client.event
-
 async def on_ready():
     print("bot is ready.")
+    async with aiohttp.ClientSession() as session:
+        channel = client.get_channel()  #fyll in drift kanal id
+        while True:
+            async with session.get('https://www.vg.no/') as r:
+                if r.status == 200: #sjekker status paa siden
+                    await channel.send('sup bro, det er abdi btw')
+                    #await asyncio.sleep(10) #Waits for 10 seconds
 
-@client.command()
+@client.command(aliases=['ey'])
 async def sup_bian(ctx):
     await ctx.send('ello')
 
@@ -16,6 +26,10 @@ async def sup_bian(ctx):
 async def spor(ctx):
     await ctx.send('security is a process, not a product')
 
+@client.command()
+async def hello(ctx):
+    ctx.channel = client.get_channel(667785200071081985)
+    await ctx.channel.send('message')
 
-
+#client.loop.create_task(on_message())
 client.run(os.getenv('DTOKEN'))
