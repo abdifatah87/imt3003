1fa4816facf579c3e69dd36725a56a7524cf4ae0
jimbob <abdifatah87@live.no>
2020-01-19 22:25:54 +0100

    background task

--- bot/bot.py
+++ bot/bot.py
@@ -6,28 +6,22 @@ import json
 
 client = commands.Bot(command_prefix = '.')
 
+timeout = 60*10     #10 minutter timeout
+
 @client.event
 async def on_ready():
     print("bot is ready.")
+
+async def background_task():
+    await client.wait_until_ready()
     async with aiohttp.ClientSession() as session:
-        channel = client.get_channel()  #fyll in drift kanal id
-        while True:
+        channel = client.get_user()     #fyll in drift kanal id
+        while not client.is_closed():
             async with session.get('https://www.vg.no/') as r:
-                if r.status == 200: #sjekker status paa siden
-                    await channel.send('sup bro, det er abdi btw')
-                    #await asyncio.sleep(10) #Waits for 10 seconds
+                if r.status == 200:     #sjekker status paa siden
+                    await channel.send('test')
+                    await asyncio.sleep(timeout)    #Waits for 10 seconds
 
-@client.command(aliases=['ey'])
-async def sup_bian(ctx):
-    await ctx.send('ello')
 
-@client.command(aliases=['what_is_security'])
-async def spor(ctx):
-    await ctx.send('security is a process, not a product')
-
-@client.command()
-async def hello(ctx):
-    ctx.channel = client.get_channel(667785200071081985)
-    await ctx.channel.send('message')
-
+client.loop.create_task(background_task())
 client.run(os.getenv('DTOKEN'))
