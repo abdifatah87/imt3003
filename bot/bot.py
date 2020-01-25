6c363f442010bc323844cd5c0c06c37d609de84b
jimbob <abdifatah87@live.no>
2020-01-25 15:05:49 +0100

    added some bash scripts

--- bot/bot.py
+++ bot/bot.py
@@ -1,21 +1,27 @@
-import discord, os
+import discord, os, subprocess
 from discord.ext import commands
 import requests
-import json
 
 client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
     print("bot is ready.")
-    errorChannel = client.get_user(249276619473027082)     #fyll in drift kanal id
-    generalChannel = client.get_user(668547115341053965)
+    #errorChannel = client.get_user(249276619473027082)     #fyll in drift kanal id
+    #generalChannel = client.get_user(668547115341053965)
     checkApache()
-
+    checkNumOfRows()
     await client.logout()
 
 def checkApache():
-    r = requests.get('http://10.212.141.148')
-    if r.status_code == 200:     #sjekker status paa siden
+    a = subprocess.check_output(['./num.sh'])
+    a = a.decode().rstrip()
+    if a == '2':
+        print("alt er oppe")
+
+def checkNumOfRows():
+    a = subprocess.check_output(['./ant.sh'])
+    a = a.decode().rstrip().replace('\n', '')[5:]
+    print(a)
 
 client.run(os.getenv('DTOKEN'))
