b0b5304fe2b736f8ea163ec0949cde4b8fc9cf8b
jimbob <abdifatah87@live.no>
2020-01-25 16:24:27 +0100

    req changes

--- bot/bot.py
+++ bot/bot.py
@@ -9,19 +9,19 @@ async def on_ready():
     print("bot is ready.")
     #errorChannel = client.get_user(249276619473027082)     #fyll in drift kanal id
     #generalChannel = client.get_user(668547115341053965)
-    checkApache()
-    checkNumOfRows()
-    await client.logout()
-
-def checkApache():
-    a = subprocess.check_output(['./num.sh'])
-    a = a.decode().rstrip()
-    if a == '2':
-        print("alt er oppe")
-
-def checkNumOfRows():
-    a = subprocess.check_output(['./ant.sh'])
-    a = a.decode().rstrip().replace('\n', '')[5:]
-    print(a)
+#    checkApache()
+#    checkNumOfRows()
+#    await client.logout()
+#
+#def checkApache():
+#    a = subprocess.check_output(['./num.sh'])
+#    a = a.decode().rstrip()
+#    if a == '2':
+#        print("alt er oppe")
+#
+#def checkNumOfRows():
+#    a = subprocess.check_output(['./ant.sh'])
+#    a = a.decode().rstrip().replace('\n', '')[5:]
+#    print(a)
 
 client.run(os.getenv('DTOKEN'))
