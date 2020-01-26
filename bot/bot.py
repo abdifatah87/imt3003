28c471032944fbbd78fcf18b483a2b91b308bd39
jimbob <abdifatah87@live.no>
2020-01-26 18:34:50 +0100

    adding scripts

--- bot/bot.py
+++ bot/bot.py
@@ -7,21 +7,25 @@ client = commands.Bot(command_prefix = '.')
 @client.event
 async def on_ready():
     print("bot is ready.")
-    #errorChannel = client.get_user(249276619473027082)     #fyll in drift kanal id
-    #generalChannel = client.get_user(668547115341053965)
-#    checkApache()
-#    checkNumOfRows()
-#    await client.logout()
-#
-#def checkApache():
-#    a = subprocess.check_output(['./num.sh'])
-#    a = a.decode().rstrip()
-#    if a == '2':
-#        print("alt er oppe")
-#
-#def checkNumOfRows():
-#    a = subprocess.check_output(['./ant.sh'])
-#    a = a.decode().rstrip().replace('\n', '')[5:]
-#    print(a)
+    errorChannel = client.get_user(249276619473027082)     #fyll in drift kanal id
+    generalChannel = client.get_user(668547115341053965)
+    checkApache()
+    checkNumOfRows()
+    await client.logout()
+
+def checkApache():
+    a = subprocess.check_output(['./num.sh'])
+    a = a.decode().rstrip()
+    if a == '2':
+        print("alt er oppe")
+
+def checkNumOfRows():
+    a = subprocess.check_output(['./ant.sh'])
+    a = a.decode().rstrip().replace('\n', '')[5:]
+    print(a)
+
+def check():
+        a = subprocess.check_output(['./ant.sh'])
+        a = a.decode().rstrip().replace('\n', '')[5:]
 
 client.run(os.getenv('DTOKEN'))
